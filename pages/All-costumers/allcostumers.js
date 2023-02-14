75c7a9ffeb64a06d4df9560117c56375fc3ef418
Tejas <tejasgiri910@gmail.com>
2023-02-14 11:38:56 +0530

    completed all costumers

--- pages/All-costumers/allcostumers.js
+++ pages/All-costumers/allcostumers.js
@@ -1,11 +1,13 @@
 const tabContainer = document.getElementById("tabsection");
 const onOpenButton = document.getElementById("opentab");
 const closeTabButton = document.getElementById("closetab");
+const main = document.getElementById("main");
 onOpenButton.addEventListener("click", function () {
   tabContainer.classList.add("opentab");
   onOpenButton.style.display = "none!important";
 
   tabContainer.classList.remove("closetab");
+  main.style.gridTemplateColumns = "1fr";
 });
 closeTabButton.addEventListener("click", function () {
   tabContainer.classList.add("closetab");
@@ -14,4 +16,5 @@ closeTabButton.addEventListener("click", function () {
     tabContainer.classList.remove("opentab");
   }, 100);
   onOpenButton.style.display = "block";
+  main.style.gridTemplateColumns = "1fr 3fr";
 });
